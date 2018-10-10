4ecef80880ff4f671b0110698bd4b17b9fd56a83
Unknown <jason.millsom@gmx.net>
2018-10-10 17:47:38 +0200

    Was able to get Pug and Style to work with express.static
    With express.static you can load all files in directories

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = 5656
+
+app.use(express.static('views'))
+app.use(express.static('styles'))
+
+app.set('view engine','pug')
+app.get('/', (req, res) => res.render('index'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
